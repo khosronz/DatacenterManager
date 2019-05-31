aa6f8cb0e121c50336974e05ad36954ea0e717ff
khosronz <khosronz.com@gmail.com>
2019-05-31 06:10:17 +0430

    Add a user by Dust testing

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends DuskTestCase
+{
+//    use DatabaseMigrations;
+    /**
+     * @group login
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testAUserCanLogin()
+    {
+        $user = factory(User::class)->create();
+
+        $this->browse(function (Browser $browser) use ($user){
+            $browser->visit('/login')
+                ->type('email',$user->email)
+                ->type('password','password')
+                ->press(__('Login'))
+                ->assertPathIs('/home');
+        });
+
+    }
+}
